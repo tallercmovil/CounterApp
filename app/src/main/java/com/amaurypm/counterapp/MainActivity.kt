d337682cc9e41f9f56dc33ad89d6b8ff2226f437
Amaury PM <amaury.pema@gmail.com>
2026-09-11 13:29:36 -0600

    Commit inicial

---
+++ app/src/main/java/com/amaurypm/counterapp/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.amaurypm.counterapp
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val tvCounter = findViewById<TextView>(R.id.tvCounter)
+
+        tvCounter.text = "Hola desde Kotlin"
+    }
+}
